2254d015318b4997a4652ec6c450a153e97cd43a
BAGTYGUL22 <bagtygulmeredova10@gmail.com>
2025-09-19 12:16:25 +0300

    update

--- src/lab01/05_initials_and_len.py
+++ src/lab01/05_initials_and_len.py
@@ -1,7 +1,4 @@
-fio = input("ФИО: ")
-fio = fio.strip()
-words = fio.split()
-initials = "".join([word[0].upper() for word in words])
-
-print("Инициалы:", initials + ".")
-print("Длина (символов):", len(fio))
+a, b, c = input().split()
+print(f"ФИО: {a} {b} {c}")
+print(f"Инициалы: {a[0]}{b[0]}{c[0]}.")
+print(f"Длина (символов): {len(a) + len(b) + len(c) + 2}")
